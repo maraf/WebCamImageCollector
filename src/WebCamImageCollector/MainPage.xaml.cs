49e87fd6dd098a1b459178087d3967de089353b6
maraf <mara@neptuo.com>
2016-06-10 21:12:59 +0200

    Initial copy from IoT samples.

---
+++ src/WebCamImageCollector/MainPage.xaml.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebCamImageCollector
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture mediaCapture;
+        private StorageFile photoFile;
+        private StorageFile recordStorageFile;
+        private StorageFile audioFile;
+        private readonly string PHOTO_FILE_NAME = "photo.jpg";
+        private readonly string VIDEO_FILE_NAME = "video.mp4";
+        private readonly string AUDIO_FILE_NAME = "audio.mp3";
+        private bool isPreviewing;
+        private bool isRecording;
+
+        #region HELPER_FUNCTIONS
+
+        enum Action
+        {
+            Enable,
+            Disable
+        }
+        /// <summary>
+        /// Helper function to enable or disable Initialization buttons
+        /// </summary>
+        /// <param name="action">enum Action</param>
+        private void SetInitButtonVisibility(Action action)
+        {
+            if (action == Action.Enable)
+            {
+                video_init.IsEnabled = true;
+            }
+            else
+            {
+                video_init.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Helper function to enable or disable video related buttons (TakePhoto, Start Video Record)
+        /// </summary>
+        /// <param name="action">enum Action</param>
+        private void SetVideoButtonVisibility(Action action)
+        {
+            if (action == Action.Enable)
+            {
+                takePhoto.IsEnabled = true;
+                takePhoto.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                takePhoto.IsEnabled = false;
+                takePhoto.Visibility = Visibility.Collapsed;
+            }
+        }
+        
+        #endregion
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            SetInitButtonVisibility(Action.Enable);
+            SetVideoButtonVisibility(Action.Disable);
+
+            isRecording = false;
+            isPreviewing = false;
+        }
+
+        private async void Cleanup()
+        {
+            if (mediaCapture != null)
+            {
+                // Cleanup MediaCapture object
+                if (isPreviewing)
+                {
+                    await mediaCapture.StopPreviewAsync();
+                    captureImage.Source = null;
+                    isPreviewing = false;
+                }
+                if (isRecording)
+                {
+                    await mediaCapture.StopRecordAsync();
+                    isRecording = false;
+                }
+                mediaCapture.Dispose();
+                mediaCapture = null;
+            }
+            SetInitButtonVisibility(Action.Enable);
+        }
+
+        /// <summary>
+        /// 'Initialize Audio and Video' button action function
+        /// Dispose existing MediaCapture object and set it up for audio and video
+        /// Enable or disable appropriate buttons
+        /// - DISABLE 'Initialize Audio and Video' 
+        /// - DISABLE 'Start Audio Record'
+        /// - ENABLE 'Initialize Audio Only'
+        /// - ENABLE 'Start Video Record'
+        /// - ENABLE 'Take Photo'
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void initVideo_Click(object sender, RoutedEventArgs e)
+        {
+            // Disable all buttons until initialization completes
+
+            SetInitButtonVisibility(Action.Disable);
+            SetVideoButtonVisibility(Action.Disable);
+
+            try
+            {
+                if (mediaCapture != null)
+                {
+                    // Cleanup MediaCapture object
+                    if (isPreviewing)
+                    {
+                        await mediaCapture.StopPreviewAsync();
+                        captureImage.Source = null;
+                        isPreviewing = false;
+                    }
+                    if (isRecording)
+                    {
+                        await mediaCapture.StopRecordAsync();
+                        isRecording = false;
+                    }
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                }
+
+                status.Text = "Initializing camera to capture audio and video...";
+                // Use default initialization
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                // Set callbacks for failure and recording limit exceeded
+                status.Text = "Device successfully initialized for video recording!";
+                mediaCapture.Failed += new MediaCaptureFailedEventHandler(mediaCapture_Failed);
+                mediaCapture.RecordLimitationExceeded += new Windows.Media.Capture.RecordLimitationExceededEventHandler(mediaCapture_RecordLimitExceeded);
+
+                // Start Preview                
+                previewElement.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+                status.Text = "Camera preview succeeded";
+
+                // Enable buttons for video and photo capture
+                SetVideoButtonVisibility(Action.Enable);
+            }
+            catch (Exception ex)
+            {
+                status.Text = "Unable to initialize camera for audio/video mode: " + ex.Message;
+            }
+        }
+
+        private void cleanup_Click(object sender, RoutedEventArgs e)
+        {
+            SetInitButtonVisibility(Action.Disable);
+            SetVideoButtonVisibility(Action.Disable);
+            Cleanup();
+        }
+        
+        /// <summary>
+        /// 'Take Photo' button click action function
+        /// Capture image to a file in the default account photos folder
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void takePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                takePhoto.IsEnabled = false;
+                captureImage.Source = null;
+
+                photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync(
+                    PHOTO_FILE_NAME, CreationCollisionOption.GenerateUniqueName);
+                ImageEncodingProperties imageProperties = ImageEncodingProperties.CreateJpeg();
+                await mediaCapture.CapturePhotoToStorageFileAsync(imageProperties, photoFile);
+                takePhoto.IsEnabled = true;
+                status.Text = "Take Photo succeeded: " + photoFile.Path;
+
+                IRandomAccessStream photoStream = await photoFile.OpenReadAsync();
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.SetSource(photoStream);
+                captureImage.Source = bitmap;
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+                Cleanup();
+            }
+            finally
+            {
+                takePhoto.IsEnabled = true;
+            }
+        }
+        
+        /// <summary>
+        /// Callback function for any failures in MediaCapture operations
+        /// </summary>
+        /// <param name="currentCaptureObject"></param>
+        /// <param name="currentFailure"></param>
+        private async void mediaCapture_Failed(MediaCapture currentCaptureObject, MediaCaptureFailedEventArgs currentFailure)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                try
+                {
+                    status.Text = "MediaCaptureFailed: " + currentFailure.Message;
+
+                    if (isRecording)
+                    {
+                        await mediaCapture.StopRecordAsync();
+                        status.Text += "\n Recording Stopped";
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    SetInitButtonVisibility(Action.Disable);
+                    SetVideoButtonVisibility(Action.Disable);
+                    status.Text += "\nCheck if camera is diconnected. Try re-launching the app";
+                }
+            });
+        }
+
+        /// <summary>
+        /// Callback function if Recording Limit Exceeded
+        /// </summary>
+        /// <param name="currentCaptureObject"></param>
+        public async void mediaCapture_RecordLimitExceeded(Windows.Media.Capture.MediaCapture currentCaptureObject)
+        {
+            try
+            {
+                if (isRecording)
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                    {
+                        try
+                        {
+                            status.Text = "Stopping Record on exceeding max record duration";
+                            await mediaCapture.StopRecordAsync();
+                            isRecording = false;
+                            if (mediaCapture.MediaCaptureSettings.StreamingCaptureMode == StreamingCaptureMode.Audio)
+                            {
+                                status.Text = "Stopped record on exceeding max record duration: " + audioFile.Path;
+                            }
+                            else
+                            {
+                                status.Text = "Stopped record on exceeding max record duration: " + recordStorageFile.Path;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            status.Text = e.Message;
+                        }
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                status.Text = e.Message;
+            }
+        }
+    }
+}
