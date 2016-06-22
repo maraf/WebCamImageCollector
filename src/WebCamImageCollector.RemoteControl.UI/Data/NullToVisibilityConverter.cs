63fdf8db301f1dd47d4867912f3862f050181bbe
maraf <mara@neptuo.com>
2016-06-22 20:10:44 +0200

    Hiding the image when source is null. Hiding the scrollbars when not needed.

---
+++ src/WebCamImageCollector.RemoteControl.UI/Data/NullToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WebCamImageCollector.RemoteControl.UI.Data
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public bool IsInverted { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (IsInverted)
+                return value != null ? Visibility.Visible : Visibility.Collapsed;
+
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
