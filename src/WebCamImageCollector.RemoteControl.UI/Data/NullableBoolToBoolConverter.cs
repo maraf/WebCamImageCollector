58228331a684facad18c0784b6ed36a803f8d32b
maraf <mara@neptuo.com>
2016-06-24 12:04:26 +0200

    Add hamburger menu.

---
+++ src/WebCamImageCollector.RemoteControl.UI/Data/NullableBoolToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace WebCamImageCollector.RemoteControl.UI.Data
+{
+    public class NullableBoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool? boolValue = value as bool?;
+            if (boolValue == null)
+                return false;
+
+            return boolValue.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Convert(value, targetType, parameter, language);
+        }
+    }
+}
---
+++ src/WebCamImageCollector.RemoteControl.UI/Menu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WebCamImageCollector.RemoteControl.UI
+{
+    public sealed partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
