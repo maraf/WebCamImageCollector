f2a07e073da6a51b5c039f317e398f672269471f
maraf <mara@neptuo.com>
2016-10-26 19:25:16 +0200

    Simplify configuration from ApplicationTriggerDetails.

--- src/WebCamImageCollector.Background/Background/Bootstrap/ApplicationTriggerConfigurationLoader.cs
+++ src/WebCamImageCollector.Background/Background/Bootstrap/ApplicationTriggerConfigurationLoader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
 
 namespace WebCamImageCollector.Background.Bootstrap
 {
@@ -13,26 +14,41 @@ namespace WebCamImageCollector.Background.Bootstrap
         {
             if (triggerDetails != null)
             {
-                object portRaw = null;
-                if (triggerDetails.Arguments.TryGetValue("Port", out portRaw) && portRaw != null)
-                    configuration.Port = Int32.Parse(portRaw.ToString());
-
-                object authTokenRaw = null;
-                if (triggerDetails.Arguments.TryGetValue("AuthenticationToken", out authTokenRaw) && authTokenRaw != null)
-                    configuration.AuthenticationToken = authTokenRaw.ToString();
-
-                object delayRaw = null;
-                if (triggerDetails.Arguments.TryGetValue("Delay", out delayRaw) && delayRaw != null)
-                    configuration.Delay = TimeSpan.FromSeconds(Int32.Parse(delayRaw.ToString()));
-
-                object intervalRaw = null;
-                if (triggerDetails.Arguments.TryGetValue("Interval", out intervalRaw) && intervalRaw != null)
+                ProcessValueInt(triggerDetails.Arguments, "Port", port => configuration.Port = port);
+                ProcessValueString(triggerDetails.Arguments, "AuthenticationToken", authToken => configuration.AuthenticationToken = authToken);
+                ProcessValueTimeSpan(triggerDetails.Arguments, "Delay", delay => configuration.Delay = delay);
+                ProcessValueTimeSpan(triggerDetails.Arguments, "Interval", interval =>
                 {
-                    TimeSpan interval = TimeSpan.FromSeconds(Int32.Parse(intervalRaw.ToString()));
                     if (interval > TimeSpan.FromSeconds(10) && interval < TimeSpan.FromMinutes(5))
                         configuration.Interval = interval;
-                }
+                });
             }
+        }
+
+        private static void ProcessValueString(ValueSet values, string key, Action<string> handler)
+        {
+            object raw = null;
+            if (values.TryGetValue(key, out raw) && raw != null)
+                handler(raw.ToString());
+        }
+
+        private static void ProcessValueInt(ValueSet values, string key, Action<int> handler)
+        {
+            ProcessValueString(values, key, raw =>
+            {
+                int value;
+                if (Int32.TryParse(raw, out value))
+                    handler(value);
+            });
+        }
+
+        private static void ProcessValueTimeSpan(ValueSet values, string key, Action<TimeSpan> handler)
+        {
+            ProcessValueInt(values, key, raw =>
+            {
+                TimeSpan value = TimeSpan.FromSeconds(raw);
+                handler(value);
+            });
         }
     }
 }
