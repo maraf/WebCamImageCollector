d8725ce518a446057a5b64dc6579a56e69b6ecce
maraf <mara@neptuo.com>
2016-10-30 19:58:34 +0100

    Add interface for common client.

---
+++ src/WebCamImageCollector.RemoteControl.UI/Services/ClientRunningInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCamImageCollector.RemoteControl.Services
+{
+    public class ClientRunningInfo
+    {
+        public bool IsRunning { get; private set; }
+        public string Message { get; private set; }
+
+        public ClientRunningInfo(bool isRunning, string message)
+        {
+            IsRunning = isRunning;
+            Message = message;
+        }
+    }
+}
---
+++ src/WebCamImageCollector.RemoteControl.UI/Services/IClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace WebCamImageCollector.RemoteControl.Services
+{
+    public interface IClient
+    {
+        string Name { get; }
+        string Url { get; }
+
+        Task<ClientRunningInfo> IsRunningAsync();
+        Task<bool> StartAsync();
+        Task<bool> StopAsync();
+        Task<ImageSource> DownloadLatest();
+    }
+}
