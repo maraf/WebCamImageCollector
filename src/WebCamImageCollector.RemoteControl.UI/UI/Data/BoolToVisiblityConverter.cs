aeb1a272712a65b578790c73dd40b2db10f5afd7
maraf <mara@neptuo.com>
2017-01-09 21:58:06 +0100

    Add some useful converters.

---
+++ src/WebCamImageCollector.RemoteControl.UI/UI/Data/BoolToVisiblityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WebCamImageCollector.RemoteControl.UI.Data
+{
+    public class BoolToVisiblityConverter : BoolConverter
+    {
+        public BoolToVisiblityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+    }
+}
