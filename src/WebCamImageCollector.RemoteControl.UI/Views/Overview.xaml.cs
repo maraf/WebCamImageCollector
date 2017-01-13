333eaef84775b1085b5872b089f0adb18b777917
maraf <mara@neptuo.com>
2017-01-13 06:48:23 +0100

    Designing RemoteClientEdit.

--- src/WebCamImageCollector.RemoteControl.UI/Views/Overview.xaml.cs
+++ src/WebCamImageCollector.RemoteControl.UI/Views/Overview.xaml.cs
@@ -20,9 +20,9 @@ namespace WebCamImageCollector.RemoteControl.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class OverviewView : Page
+    public sealed partial class Overview : Page
     {
-        public OverviewView()
+        public Overview()
         {
             this.InitializeComponent();
         }
