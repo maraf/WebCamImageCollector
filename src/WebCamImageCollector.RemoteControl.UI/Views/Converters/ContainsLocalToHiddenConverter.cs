5873827eaec442b64ff3779190ea19db1ac19eb3
Marek Fišera <mara@neptuo.com>
2017-08-31 20:23:19 +0200

    Hide 'Add Local' command when local client is already created.

---
+++ src/WebCamImageCollector.RemoteControl.UI/Views/Converters/ContainsLocalToHiddenConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCamImageCollector.RemoteControl.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace WebCamImageCollector.RemoteControl.Views.Converters
+{
+    public class ContainsLocalToHiddenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            IEnumerable<ClientOverviewViewModel> models = (IEnumerable<ClientOverviewViewModel>)value;
+            return models.Any(m => !m.IsRemote) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
