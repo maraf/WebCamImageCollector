169c1beb74a754218adb7d82a247d8f7487c3f2c
Marek Fišera <mara@neptuo.com>
2017-09-01 00:15:36 +0200

    Move converters to new Views folder.

--- src/WebCamImageCollector.RemoteControl.UI/Views/Converters/NullToVisibilityConverter.cs
+++ src/WebCamImageCollector.RemoteControl.UI/Views/Converters/NullToVisibilityConverter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
-namespace WebCamImageCollector.RemoteControl.UI.Data
+namespace WebCamImageCollector.RemoteControl.Views.Converters
 {
     public class NullToVisibilityConverter : IValueConverter
     {
